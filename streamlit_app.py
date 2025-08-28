cb03e39b6b3def979d75cbb6b61eccfb8cfbcf29
kieranadair <152869431+kieranadair@users.noreply.github.com>
2025-08-28 14:28:09 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,15 @@ else:
     st.write("Click the button to make me say hello")
 
 # Example 1
-st.subheader('Display text')
+st.subheader('Display text using st.write()')
 st.write('Hello, *World!* :sunglasses:')
 
 # Example 2
-st.subheader('Display numbers')
+st.subheader('Display numbers too')
 st.write(1234)
 
 # Example 3
-st.subheader('Display DataFrame')
+st.subheader('Display a DataFrame WOW!')
 df = pd.DataFrame({
      'first column': [1, 2, 3, 4],
      'second column': [10, 20, 30, 40]
@@ -30,16 +30,17 @@ df = pd.DataFrame({
 st.write(df)
 
 # Example 4
-st.subheader('Accept multiple arguments')
+st.subheader('Accept multiple arguments as comma separated args with st.write(a,b,c)')
 st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
 
 # Example 5
-st.subheader('Display charts')
+st.subheader('Display charts build in altair then called with st.write(chart var)')
 df2 = pd.DataFrame(
      np.random.randn(200, 3),
      columns=['a', 'b', 'c'])
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
 
 
